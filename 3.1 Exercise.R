2c7953613212f509c4589a9c60621b88188b893e
Jesus Flores <jesus.flores.p19@gmail.com>
2023-05-16 02:21:25 -0600

    Finish exercies 3.1, 3.3, 3.5

--- 3.1 Exercise.R
+++ 3.1 Exercise.R
@@ -1,15 +1,33 @@
 # Create a vector x storing the sequence 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20.
 
+x <- 10:20
+
 # Add 2 to it. Save the result into a variable called y.
+
+y <- x+2
 
 # Multiply y by 3. Save that into a variable called z.
 
+z <- y*3
+
 # Subtract 6 from z and divide the result by 3. Save what you get into a variable called answer.
+
+answer <- z-6
+answer <- answer/3
 
 # Print your answer variable.
 
+print(answer)
+
 #Try to do the entire operation in a single line of code. 
 
+answer.sec <- (((x+2)*3)-6)/3
+
 # What do you need to do to get the same result? 
+## First we need respect the same proceeds of operations, this can only do if understand the order.
+
 # Why is that? 
+## Because R have certain order in operations. And if we want get the same result we need understand it.
+
 # Do you notice anything about the operations? Do they follow a specific order; how are they carried out?
+## Fist Division and Multiplication, then Substraction and Addition. This is the order.
--- 3.3 Exercise.R
+++ 3.3 Exercise.R
@@ -10,8 +10,12 @@
 #5-Headed Dragon
 #Exodia the Forbidden One
 #Dragon Master Knight
+cards <- c("Blue-Eyes White Dragon", "Exodius", "The Winged Dragon of Ra", "Raigeki", 
+           "Slifer the Sky Dragon", "Obelisk the Tormentor", "Black Luster Soldier", 
+           "5-Headed Dragon", "Exodia the Forbidden One", "Dragon Master Knight")
 
 # Check the type of cards.
+typeof(cards)
 
 # Create a vector called atk and assign to it the following values:
   
@@ -27,13 +31,25 @@
 #1000
 #5000
 
+atk <- c(3000, NA, NA, NA, NA, 4000, 3000, 5000, 1000, 5000)
+
 # Check the type of atk.
+typeof(atk)
 
 # Use the combine function to combine cards and atk into a single vector called yugioh.
+yugioh <- c(cards, atk)
 
 # Check the type of the new object.
+typeof(yugioh)
 
 # What conclusions can you draw from this? 
-# If we had added TRUE/FALSE values to the atk vector, what would have the print(atk) command returned? 
-# What about print(yugioh)?
+## When combine the first vector type characther and second vector type double. R converted all in the same type, in this case into characther.
 
+# If we had added TRUE/FALSE values to the atk vector, what would have the print(atk) command returned? 
+## Maybe in boolean. I do the exercise to validate:
+atk.sec <- c(T,T,T,T,T,F,F)
+typeof(atk.sec)
+## Print TRUE/FALSE values, and the type is LOGICAL.
+
+# What about print(yugioh)?
+## Its the same, characther.
